e83e910235b483b71b0c2c957d96adf234b7b7cf
Борис <102375597+Koshkinkhost@users.noreply.github.com>
2025-03-04 21:27:32 +0300

    messagebox added

--- Innokentiy_WinForms/Shop.cs
+++ Innokentiy_WinForms/Shop.cs
@@ -17,6 +17,38 @@ namespace Innokentiy_WinForms
         {
             InitializeComponent();
         }
+        public void Add_Gun(int damage, int weight, double price)
+        {
+            if (GameProvider.hero.Wallet >= price)
+            {
+                Weapon weapon = new Weapon(damage, weight, price);
+                GameProvider.hero.Items.Add(weapon);
+                GameProvider.hero.Wallet -= price;
+                label_money.Text = (int.Parse(label_money.Text) - weapon.Price).ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("Недостаточно денег");
+            }
+
+        }
+        
+        public void Buy_Potion(double weight, double price)
+        {
+            if (GameProvider.hero.Wallet >= price)
+            {
+                Potion potion = new Potion(weight, price);
+                GameProvider.hero.Items.Add(potion);
+                GameProvider.hero.Wallet -= price;
+                label_money.Text = (int.Parse(label_money.Text) - potion.Price).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Недостаточно денег");
+            }
+
+        }
 
         private void Shop_Load(object sender, EventArgs e)
         {
@@ -25,44 +57,47 @@ namespace Innokentiy_WinForms
 
         private void button_buy1_Click(object sender, EventArgs e)
         {
-            Potion potion = new Potion(5, 100);
-            GameProvider.hero.Items.Add(potion);
-            label_money.Text = (int.Parse(label_money.Text) - potion.Price).ToString();
+            Buy_Potion(5, 100);
+
         }
 
         private void button_buy2_Click(object sender, EventArgs e)
         {
-            Potion potion = new Potion(5, 110);
-            GameProvider.hero.Items.Add(potion);
-            label_money.Text = (int.Parse(label_money.Text) - potion.Price).ToString();
+            Buy_Potion(10, 120);
+
         }
 
         private void button_buy3_Click(object sender, EventArgs e)
         {
-            Potion potion = new Potion(5, 120);
-            GameProvider.hero.Items.Add(potion);
-            label_money.Text = (int.Parse(label_money.Text) - potion.Price).ToString();
+            Buy_Potion(5, 80);
+
         }
 
         private void button_gun1_Click(object sender, EventArgs e)
         {
-            Weapon weapon = new Weapon(20, 30, 230);
-            GameProvider.hero.Items.Add(weapon);
-            label_money.Text = (int.Parse(label_money.Text) - weapon.Price).ToString();
+            Add_Gun(20, 30, 230);
         }
 
         private void button_gun2_Click(object sender, EventArgs e)
         {
-            Weapon weapon = new Weapon(20, 30, 240);
-            GameProvider.hero.Items.Add(weapon);
-            label_money.Text = (int.Parse(label_money.Text) - weapon.Price).ToString();
+            Add_Gun(20, 30, 240);
         }
 
         private void button_gun3_Click(object sender, EventArgs e)
         {
-            Weapon weapon = new Weapon(20, 30, 250);
-            GameProvider.hero.Items.Add(weapon);
-            label_money.Text = (int.Parse(label_money.Text) - weapon.Price).ToString();
+            Add_Gun(20, 40, 250);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           var result= MessageBox.Show("Уже уходите?","Предупреждение",MessageBoxButtons.OKCancel,MessageBoxIcon.Exclamation);
+            if (result == DialogResult.Yes)
+            {
+                this.Hide();
+                Start s=new Start();
+                s.Show();
+            }
+            
         }
     }
 }
